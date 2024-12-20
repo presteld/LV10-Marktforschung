bdc411f1ce7e56204e1fa906ec019a4b52c1c8ec
presteld <dietmar.prestel@hs-kempten.de>
2024-12-20 16:38:58 +0100

    Version 2 GUI fertig

--- src/Marktforschung.java
+++ src/Marktforschung.java
@@ -4,11 +4,28 @@ import java.awt.*;
 public class Marktforschung extends JFrame {
 
     private JPanel jpPanel;
+    private JLabel lblName;
+    private JLabel lblDatum;
+    private JLabel lblBetreuung;
+    private JLabel lblQualitaet;
+    private JLabel lblTermintreue;
+    private JLabel lblPreis;
+    private JTextField txtName;
+    private JTextField txtDatum;
+    private JComboBox cboBetreuung;
+    private JComboBox cboQualitaet;
+    private JComboBox cboTermintreue;
+    private JComboBox cboPreis;
+    private JCheckBox cbLetzte30Tage;
+    private JButton btnErfassen;
+    private JButton btnBerechnen;
+    private JButton btnAlleAusgeben;
+    private JTextArea tyAusgabe;
 
     public Marktforschung() throws HeadlessException {
         setTitle("Markforschung");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600,500);
+        setSize(1000,500);
         setContentPane(jpPanel);
         setVisible(true);
 
