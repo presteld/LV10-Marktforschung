2c3c99ad8f8bac15e4908e294d3780c4052d2202
presteld <dietmar.prestel@hs-kempten.de>
2024-12-20 16:48:39 +0100

    Version 3 Klasse Bewertung erstellt

---
+++ src/Bewertung.java
@@ -0,0 +1,44 @@
+import java.time.LocalDateTime;
+
+public class Bewertung {
+    private String sName;
+    private LocalDateTime ldtDatum;
+    private int iBetreuung;
+    private int iQualitaet;
+    private int iTermintreue;
+    private int iPreis;
+
+
+    public Bewertung(String sName, LocalDateTime ldtDatum, int iBetreuung, int iQualitaet, int iTermintreue, int iPreis) {
+        this.sName = sName;
+        this.ldtDatum = ldtDatum;
+        this.iBetreuung = iBetreuung;
+        this.iQualitaet = iQualitaet;
+        this.iTermintreue = iTermintreue;
+        this.iPreis = iPreis;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public LocalDateTime getLdtDatum() {
+        return ldtDatum;
+    }
+
+    public int getiBetreuung() {
+        return iBetreuung;
+    }
+
+    public int getiQualitaet() {
+        return iQualitaet;
+    }
+
+    public int getiTermintreue() {
+        return iTermintreue;
+    }
+
+    public int getiPreis() {
+        return iPreis;
+    }
+}
