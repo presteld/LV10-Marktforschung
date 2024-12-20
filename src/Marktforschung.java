ff38b9bb6941d7890c032392fc23d04da96b340b
presteld <dietmar.prestel@hs-kempten.de>
2024-12-20 17:20:26 +0100

    Version 4 Arraylist erstellt

--- src/Marktforschung.java
+++ src/Marktforschung.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Marktforschung extends JFrame {
 
@@ -20,7 +25,9 @@ public class Marktforschung extends JFrame {
     private JButton btnErfassen;
     private JButton btnBerechnen;
     private JButton btnAlleAusgeben;
-    private JTextArea tyAusgabe;
+    private JTextArea taAusgabe;
+
+    private ArrayList<Bewertung> alBewertungen = new ArrayList<Bewertung>();
 
     public Marktforschung() throws HeadlessException {
         setTitle("Markforschung");
@@ -30,7 +37,51 @@ public class Marktforschung extends JFrame {
         setVisible(true);
 
 
+        alBewertungen.add(new Bewertung("Jasmin K.",LocalDateTime.of(2024,12,19,0,0,0),2,4,5,1));
+        alBewertungen.add(new Bewertung("Roland K.",LocalDateTime.of(2024,11,17,0,0,0),2,3,5,1));
+        alBewertungen.add(new Bewertung("Helga K.",LocalDateTime.of(2024,9,17,0,0,0),1,3,5,1));
+        btnErfassen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                erfassen();
+            }
+        });
+        btnAlleAusgeben.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                alleAusgeben();
+            }
+        });
     }
+
+
+
+    private void alleAusgeben() {
+        taAusgabe.setText("Folgende Bewertungen wurden gespeichert:\n\n");
+
+        for (Bewertung bewB: alBewertungen){
+
+            taAusgabe.append("\n" +   bewB.getsName() + "\t" +    bewB.getiBetreuung() + "\t" +    bewB.getiQualitaet() +"\t"  + bewB.getiTermintreue() + "\t" + bewB.getiPreis()+ "\t"+ bewB.getLdtDatum() );
+        }
+
+    }
+
+    public void erfassen() {
+        String sName = txtName.getText().toString();
+
+        String sDatum = txtDatum.getText().toString();
+        LocalDateTime ldtDatum = LocalDate.parse(sDatum).atStartOfDay();
+
+        int iBetreuung = Integer.parseInt(cboBetreuung.getSelectedItem().toString());
+        int iQualitaet = Integer.parseInt(cboQualitaet.getSelectedItem().toString());
+        int iTermintreue = Integer.parseInt(cboTermintreue.getSelectedItem().toString());
+        int iPreis = Integer.parseInt(cboPreis.getSelectedItem().toString());
+
+        Bewertung bewKunde = new Bewertung(sName,ldtDatum,iBetreuung,iQualitaet,iTermintreue,iPreis);
+        alBewertungen.add(bewKunde);
+
+    }
+
 
     public static void main(String[] args) {
        new Marktforschung();
